498d32ef36aebaca72ffe26c9089da2dd7914607
Zach Beiser <zbeiser@gmail.com>
2022-12-14 14:13:10 -0800

    installed uuid npm for number generation and imported helper folder with fsUtils.js to help with fs functions

--- routes/notes.js
+++ routes/notes.js
@@ -1,3 +1,11 @@
 const notes = require('express').Router();
+const { v4: uuidv4 } = require('uuid');
+const {
+  readFromFile,
+  readAndAppend,
+  writeToFile,
+} = require('../helpers/fsUtils');
+
+
 
 module.exports = notes;
