8a11bc41f21ff82f586fa71386cfe669cee602e3
Zach Beiser <zbeiser@gmail.com>
2022-12-14 14:52:38 -0800

    fixed note id naming

--- routes/notes.js
+++ routes/notes.js
@@ -19,7 +19,7 @@ notes.post('/', (req, res) => {
     const newNote = {
       title,
       text,
-      note_id: uuidv4(),
+      id: uuidv4(),
     };
 
     readAndAppend(newNote, './db/db.json');
