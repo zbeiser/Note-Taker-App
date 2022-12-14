ec45f18cc696c223231de8f33d916a41a5024452
Zach Beiser <zbeiser@gmail.com>
2022-12-14 14:39:25 -0800

    added POST route to notes.js for adding a new note to the database

--- routes/notes.js
+++ routes/notes.js
@@ -11,4 +11,22 @@ notes.get('/', (req, res) => {
   readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
 
+// POST route for adding a new note to database
+notes.post('/', (req, res) => {
+  const { title, text } = req.body;
+
+  if (req.body) {
+    const newNote = {
+      title,
+      text,
+      note_id: uuidv4(),
+    };
+
+    readAndAppend(newNote, './db/db.json');
+    res.json(`Note added successfully`);
+  } else {
+    res.error('Error in adding note');
+  }
+});
+
 module.exports = notes;
