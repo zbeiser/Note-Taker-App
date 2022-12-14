6640a3fd27c8ae6c96dcf7787a43c3316a68a262
Zach Beiser <zbeiser@gmail.com>
2022-12-14 13:22:36 -0800

    added routes to server.js

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// Importing requirements
 const express = require('express');
 const path = require('path');
 const api = require('./routes/index.js');
@@ -6,13 +7,28 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+// Middleware for parsing JSON responses, urlencoded data, prepending api to routes, 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api', api);
 app.use(express.static('public'));
 
+// Get route for homepage
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
 
+// GET route for notes page
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
 
+// Wildcard route redirects to homepage
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/index.html'))
+);
+
+// Tells where to listen for the server
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
